c13a549717a0db172152c1d8ea8a73fff7784515
Marcos Vinicius <manteguinha.mantega@gmail.com>
2023-11-15 21:25:09 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const html = document.documentElement
+const typewriterElement = document.querySelector('.typewriter')
+
+const img = document.querySelector('#profile img')
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+    img.setAttribute('src', 'https://avatars.githubusercontent.com/u/63943591?v=4')
+} else {
+    img.setAttribute('src', 'https://avatars.githubusercontent.com/u/63943591?v=4')
+}
+
+if (typewriterElement) {
+    const linguagens_inicio = typewriterElement.innerHTML
+    typewriterElement.innerHTML = ''
+
+    typeWriter(linguagens_inicio, 0)
+}
+
+window.matchMedia('(prefers-color-scheme: light)').addListener(event => {
+    const theme = event.matches ? 'light' : 'dark'
+
+    if (theme === 'light') {
+        img.setAttribute('src', 'https://avatars.githubusercontent.com/u/63943591?v=4')
+    } else {
+        img.setAttribute('src', 'https://avatars.githubusercontent.com/u/63943591?v=4')
+    }
+})
+
+function switchTheme() {
+    html.classList.toggle('lightTheme')
+
+    if (html.classList.contains('lightTheme')) {
+        img.setAttribute('src', 'https://avatars.githubusercontent.com/u/63943591?v=4')
+    } else {
+        img.setAttribute('src', 'https://avatars.githubusercontent.com/u/63943591?v=4')
+    }
+}
+
+function share() {
+    try {
+        if (navigator.share) {
+            navigator.share({
+                title: 'Marcos Vinicius | MVMS',
+                url: 'https://noowz-devlinks.vercel.app',
+            })
+
+            console.info('You have shared the link successfully!')
+        } else {
+            alert('Your browser does not support the Web Share API!')
+        }
+    } catch (error) {
+        console.error(`An error occured while using the Web share API!\nError: ${error}`)
+    }
+}
+
+function typeWriter(linguagens_inicio, i) {
+    if (i < linguagens_inicio.length) {
+        typewriterElement.innerHTML += linguagens_inicio.charAt(i)
+        setTimeout(() => typeWriter(linguagens_inicio, i + 1), 50)
+    }
+}
